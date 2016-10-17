ddabef3d384ea0da94b058eb011f32e3c9685e47
Ircama <Ircama@users.noreply.github.com>
2016-10-17 18:45:43 +0200

    Original drop
    QR-code generator (Bar generator) by Dariusz Rorat (dariuszrorat).
    http://sourceforge.net/users/dariuszrorat
    
    Code for the Lazarus IDE.
    
    Imported from sourceforge.
    https://sourceforge.net/projects/bargenerator/
    
     Changes to be committed:
    	new file:   README.md
    	new file:   bar_generator.ico
    	new file:   bar_generator.lpi
    	new file:   bar_generator.lpr
    	new file:   bar_generator.res
    	new file:   languages/lclstrconsts.pl.po
    	new file:   languages/messages.pl.po
    	new file:   programconsts.pas
    	new file:   srcmain.lfm
    	new file:   srcmain.pas

---
+++ README.md
@@ -0,0 +1,17 @@
+# QR-code generator (Bar generator)
+
+QR-code generator (Bar generator)  by Dariusz Rorat ([dariuszrorat](http://sourceforge.net/users/dariuszrorat)).
+
+Code for the [Lazarus](http://www.lazarus-ide.org/) IDE.
+
+Imported from [sourceforge](https://sourceforge.net/projects/bargenerator/).
+
+QR Code, Aztec and Data Matrix Code generator.
+
+Original screenshots:
+
+![QR Code text](https://cloud.githubusercontent.com/assets/8292987/19453453/936c8098-94b5-11e6-9a9c-14a1a59f5f27.png)
+
+![Aztec Code text](https://cloud.githubusercontent.com/assets/8292987/19453454/936f8a68-94b5-11e6-859c-a447a55456b9.png)
+
+![Data Matrix Code text](https://cloud.githubusercontent.com/assets/8292987/19453452/9364b62e-94b5-11e6-9d02-2c1e715f3d73.png)
---
+++ programconsts.pas
@@ -0,0 +1,42 @@
+{
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.        *
+ *                                                                         *
+ ***************************************************************************
+
+  Author: Dariusz Rorat
+
+}
+
+unit ProgramConsts;
+{$mode objfpc}{$H+}
+interface
+
+resourcestring
+
+  strCaption = 'Bar code generator';
+  strCodeType = 'Code type';
+  strEccLevel = 'ECC Level';
+  strLoadFromText = 'Load text';
+  strQuit = 'Quit';
+  strFilter = 'Text files (*.txt)|*.txt|All files (*.*)|*.*';
+  strOpenDialogTitle = 'Open existing file';
+  strConvertError = 'Could not to convert text';
+
+implementation
+
+end.
